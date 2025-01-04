0ed72a5e787e37ee7e87c09ed7f1a1d2c76acc9f
saadixsd <160436315+saadixsd@users.noreply.github.com>
2025-01-04 12:38:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 
 # Use a single Flask app instance
 app = Flask(__name__)
+app.run(host='0.0.0.0', port=5000)
 CORS(app)  # Add this line to enable CORS
 
 # Configure logging
