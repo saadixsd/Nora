7598d8dd4af1de7909b28ae5b69e5d1a130eebe3
saadixsd <160436315+saadixsd@users.noreply.github.com>
2025-01-05 16:50:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import seaborn as sns
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5500)
 
 
 CORS(app, resources={r"/*": {"origins": ["https://xenoraai.com"]}})
