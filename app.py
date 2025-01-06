437aa1ccb9e69a82da5fff9b099486d6d460d23a
saadixsd <160436315+saadixsd@users.noreply.github.com>
2025-01-06 13:06:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def home():
     return "Welcome to Nora!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5500)
+    app.run(host='0.0.0.0', port=5000)
 
 
 CORS(app, resources={r"/*": {"origins": ["https://xenoraai.com"]}})
